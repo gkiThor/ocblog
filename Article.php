f8136ee915618ffbae1ff3f59e6dd13c052962d5
kithor <soulichanh.kithor@gmail.com>
2020-09-07 11:10:43 +0200

    creation de la methode getArticles() recupere tous les articles

--- Article.php
+++ Article.php
@@ -2,5 +2,11 @@
 
 class Article
 {
-    
+    public function getArticles()
+    {
+        $db = new Database();
+        $connection = $db->getConnection();
+        $result = $connection->query('SELECT id, title, content, author, createdAt FROM article ORDER BY id DESC');
+        return $result;
+    }
 }
