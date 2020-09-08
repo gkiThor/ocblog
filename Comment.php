b52f5e67fe6809759ba742b399201035004cf04d
kithor <soulichanh.kithor@gmail.com>
2020-09-08 18:20:18 +0200

    ajout de la methode getCommentsFromArticle() en parametre id de l'article

--- Comment.php
+++ Comment.php
@@ -2,5 +2,9 @@
 
 class Comment extends Database
 {
-    
+    public function getCommentsFromArticle($articleId)
+    {
+        $sql = 'SELECT id, pseudo, content, createdAt FROM comment WHERE article_id = ? ORDER BY createdAt DESC';
+        return $this->createQuery($sql, [$articleId]);
+    }
 }
