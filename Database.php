92a472e4518641dfe4e6b3b5652e4ba029a7810f
kithor <soulichanh.kithor@gmail.com>
2020-09-08 10:40:33 +0200

    ajout de la propriete private à  et methode checkConnection() pour verifier la connection

--- Database.php
+++ Database.php
@@ -5,17 +5,29 @@ class Database
     //Nos constantes
     const DB_HOST = 'mysql:host=localhost;dbname=blog;charset=utf8';
     const DB_USER = 'root';
-    const DB_PASS = 'root';
+    const DB_PASS = '';
+
+    private $connection;
+
+    private function checkConnection()
+    {
+        //Vérifie si la connexion est nulle et fait appel à getConnection()
+        if($this->connection === null) {
+            return $this->getConnection();
+        }
+        //Si la connexion existe, elle est renvoyée, inutile de refaire une connexion
+        return $this->connection;
+    }
 
     //Méthode de connexion à notre base de données
     public function getConnection()
     {
         //Tentative de connexion à la base de données
         try{
-            $connection = new PDO(self::DB_HOST, self::DB_USER, self::DB_PASS);
-            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->connection = new PDO(self::DB_HOST, self::DB_USER, self::DB_PASS);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             //On renvoie la connexion
-            return $connection;
+            return $this->connection;
         }
         //On lève une erreur si la connexion échoue
         catch(Exception $errorConnection)
@@ -29,11 +41,11 @@ class Database
     {
         if($parameters)
         {
-            $result = $this->getConnection()->prepare($sql);
+            $result = $this->checkConnection()->prepare($sql);
             $result->execute($parameters);
             return $result;
         }
-        $result = $this->getConnection()->query($sql);
+        $result = $this->checkConnection()->query($sql);
         return $result;
     }
 }
