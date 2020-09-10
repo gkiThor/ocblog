f5905a02c3e5f85c0707807e4eee401b6c2928ba
kithor <soulichanh.kithor@gmail.com>
2020-09-10 14:16:13 +0200

    suppression des constantes d'identification dans le fichier DAO.php et ajout de nouvelles constantes dans dev.php

--- config/dev.php
+++ config/dev.php
@@ -1,5 +1,8 @@
 <?php
 
-const DB_HOST = 'mysql:host=localhost;dbname=blog;charset=utf8';
+const HOST = 'localhost';
+const DB_NAME = 'blog';
+const CHARSET = 'utf8';
+const DB_HOST = 'mysql:host='.HOST.';dbname='.DB_NAME.';charset='.CHARSET;
 const DB_USER = 'root';
 const DB_PASS = '';
