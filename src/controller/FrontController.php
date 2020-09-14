7e6c9f49ef3d9807084f8600484fe59eec12c02f
kithor <soulichanh.kithor@gmail.com>
2020-09-14 22:52:43 +0200

    ajout du namespace et appel de classe ArticleDAO

--- src/controller/FrontController.php
+++ src/controller/FrontController.php
@@ -1,11 +1,14 @@
 <?php
 
 namespace App\src\controller;
+use App\src\DAO\ArticleDAO;
 
 class FrontController
 {
     public function home()
     {
+        $article = new ArticleDAO();
+        $articles = $article->getArticles();
         require '../templates/home.php';
     }
 }
