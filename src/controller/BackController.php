8c19499ff3ba486981b7993e85b85ada059f56cd
kithor <soulichanh.kithor@gmail.com>
2020-10-07 00:53:12 +0200

    ajout de l'appel de la méthode validate() depuis la méthode addArticle() de la classe BackController.php

--- src/controller/BackController.php
+++ src/controller/BackController.php
@@ -9,13 +9,18 @@ class BackController extends Controller
     public function addArticle(Parameter $post)
     {
         if($post->get('submit')) {
-            $this->articleDAO->addArticle($post);
-            $this->session->set('add_article', 'Le nouvel article a bien été ajouté');
-            header('Location: ../public/index.php');
+            $errors = $this->validation->validate($post, 'Article');
+            if(!$errors) {
+                $this->articleDAO->addArticle($post);
+                $this->session->set('add_article', 'Le nouvel article a bien été ajouté');
+                header('Location: ../public/index.php');
+            }
+            return $this->view->render('add_article', [
+                'post' => $post,
+                'errors' => $errors
+            ]);
         }
-        return $this->view->render('add_article', [
-            'post' => $post
-        ]);
+        return $this->view->render('add_article');
     }
 
     public function editArticle(Parameter $post, $articleId)
