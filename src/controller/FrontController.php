2f954572295aef9998c79b56f5f517e32ef9983a
kithor <soulichanh.kithor@gmail.com>
2020-10-09 11:30:43 +0200

    ajout de l'appel à la méthode validate() dans la classe FrontController

--- src/controller/FrontController.php
+++ src/controller/FrontController.php
@@ -27,9 +27,20 @@ class FrontController extends Controller
     public function addComment(Parameter $post, $articleId)
     {
         if($post->get('submit')) {
-            $this->commentDAO->addComment($post, $articleId);
-            $this->session->set('add_comment', 'Le nouveau commentaire a bien été ajouté');
-            header('Location: ../public/index.php?route=article&articleId='.$articleId);
+            $errors = $this->validation->validate($post, 'Comment');
+            if(!$errors) {
+                $this->commentDAO->addComment($post, $articleId);
+                $this->session->set('add_comment', 'Le nouveau commentaire a bien été ajouté');
+                header('Location: ../public/index.php');
+            }
+            $article = $this->articleDAO->getArticle($articleId);
+            $comments = $this->commentDAO->getCommentsFromArticle($articleId);
+            return $this->view->render('single', [
+                'article' => $article,
+                'comments' => $comments,
+                'post' => $post,
+                'errors' => $errors
+            ]);
         }
     }
 }
