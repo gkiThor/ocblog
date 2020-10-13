7e69eda174475d79f9fec62081341f74ec1bc462
kithor <soulichanh.kithor@gmail.com>
2020-10-13 23:24:57 +0200

    adding validation constraints in register.php file

--- templates/register.php
+++ templates/register.php
@@ -4,9 +4,11 @@
 <div>
     <form method="post" action="../public/index.php?route=register">
         <label for="pseudo">Pseudo</label><br>
-        <input type="text" id="pseudo" name="pseudo"><br>
+        <input type="text" id="pseudo" name="pseudo" value="<?= isset($post) ? htmlspecialchars($post->get('pseudo')): ''; ?>"><br>
+        <?= isset($errors['pseudo']) ? $errors['pseudo'] : ''; ?>
         <label for="password">Mot de passe</label><br>
         <input type="password" id="password" name="password"><br>
+        <?= isset($errors['password']) ? $errors['password'] : ''; ?>
         <input type="submit" value="Inscription" id="submit" name="submit">
     </form>
     <a href="../public/index.php">Retour à l'accueil</a>
