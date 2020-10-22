917bfcc413d34f80b2fe9f954c67097422beef5b
kithor <soulichanh.kithor@gmail.com>
2020-10-22 15:25:07 +0200

    adding the login() method in the UserDAO class and check if the nickname exists in the database

--- src/DAO/UserDAO.php
+++ src/DAO/UserDAO.php
@@ -22,4 +22,16 @@ class UserDAO extends DAO
             return '<p>Le pseudo existe déjà</p>';
         }
     }
+
+    public function login(Parameter $post)
+    {
+        $sql = 'SELECT id, password FROM user WHERE pseudo = ?';
+        $data = $this->createQuery($sql, [$post->get('pseudo')]);
+        $result = $data->fetch();
+        $isPasswordValid = password_verify($post->get('password'), $result['password']);
+        return [
+            'result' => $result,
+            'isPasswordValid' => $isPasswordValid
+        ];
+    }
 }
