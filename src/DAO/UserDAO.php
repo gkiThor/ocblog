382120173b469d108fdb3aeba2698c37497ce266
kithor <soulichanh.kithor@gmail.com>
2020-10-28 23:46:14 +0100

    update UserDAO class in the login() method

--- src/DAO/UserDAO.php
+++ src/DAO/UserDAO.php
@@ -25,7 +25,7 @@ class UserDAO extends DAO
 
     public function login(Parameter $post)
     {
-        $sql = 'SELECT id, password FROM user WHERE pseudo = ?';
+        $sql = 'SELECT user.id, user.role_id, user.password, role.name FROM user INNER JOIN role ON role.id = user.role_id WHERE pseudo = ?';
         $data = $this->createQuery($sql, [$post->get('pseudo')]);
         $result = $data->fetch();
         $isPasswordValid = password_verify($post->get('password'), $result['password']);
