c383113006e395504fee78da669542b1ec51d0c5
kithor <soulichanh.kithor@gmail.com>
2020-11-03 12:23:52 +0100

    added the unflagComment method into the CommentDAO class

--- src/DAO/CommentDAO.php
+++ src/DAO/CommentDAO.php
@@ -42,6 +42,12 @@ class CommentDAO extends DAO
         $sql = 'UPDATE comment SET flag = ? WHERE id = ?';
         $this->createQuery($sql, [1, $commentId]);
     }
+    
+    public function unflagComment($commentId)
+    {
+        $sql = 'UPDATE comment SET flag = ? WHERE id = ?';
+        $this->createQuery($sql, [0, $commentId]);
+    }
 
     public function deleteComment($commentId)
     {
