5b813260e7dc1c278f4c0fe7a20c4d853828b689
kithor <soulichanh.kithor@gmail.com>
2020-11-03 12:20:10 +0100

    update the administration view for the report comment

--- templates/administration.php
+++ templates/administration.php
@@ -37,5 +37,29 @@
 </table>
 
 <h2>Commentaires signalés</h2>
+<table>
+    <tr>
+        <td>Id</td>
+        <td>Pseudo</td>
+        <td>Message</td>
+        <td>Date</td>
+        <td>Actions</td>
+    </tr>
+    <?php
+    foreach ($comments as $comment)
+    {
+        ?>
+        <tr>
+            <td><?= htmlspecialchars($comment->getId());?></td>
+            <td><?= htmlspecialchars($comment->getPseudo());?></td>
+            <td><?= substr(htmlspecialchars($comment->getContent()), 0, 150);?></td>
+            <td>Créé le : <?= htmlspecialchars($comment->getCreatedAt());?></td>
+            <td>En construction</td>
+        </tr>
+        <?php
+    }
+    ?>
+</table>
+
 
 <h2>Utilisateurs</h2>
