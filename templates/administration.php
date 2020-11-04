e7b2dba957b07704f92e4a0ae0a79141b36205db
kithor <soulichanh.kithor@gmail.com>
2020-11-04 15:49:55 +0100

    update the administration view

--- templates/administration.php
+++ templates/administration.php
@@ -66,5 +66,27 @@
     ?>
 </table>
 
-
 <h2>Utilisateurs</h2>
+<table>
+    <tr>
+        <td>Id</td>
+        <td>Pseudo</td>
+        <td>Date</td>
+        <td>Rôle</td>
+        <td>Actions</td>
+    </tr>
+    <?php
+    foreach ($users as $user)
+    {
+        ?>
+        <tr>
+            <td><?= htmlspecialchars($user->getId());?></td>
+            <td><?= htmlspecialchars($user->getPseudo());?></td>
+            <td>Créé le : <?= htmlspecialchars($user->getCreatedAt());?></td>
+            <td><?= htmlspecialchars($user->getRole());?></td>
+            <td>En construction</td>
+        </tr>
+        <?php
+    }
+    ?>
+</table>
