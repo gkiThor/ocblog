8bc16ae9e7e47a3675e6724d5d3fb2018dda155f
kithor <soulichanh.kithor@gmail.com>
2020-11-04 15:47:33 +0100

    call the getUsers method into the admnistration method in the BackController class

--- src/controller/BackController.php
+++ src/controller/BackController.php
@@ -10,9 +10,12 @@ class BackController extends Controller
     {
         $articles = $this->articleDAO->getArticles();
         $comments = $this->commentDAO->getFlagComments();
+        $users = $this->userDAO->getUsers();
+
         return $this->view->render('administration', [
             'articles' => $articles,
-            'comments' => $comments
+            'comments' => $comments,
+            'users' => $users
         ]);
     }
 
