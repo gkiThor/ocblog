7b3440dcd1630902937e6ff779d94d2156cb2d6d
gsoulichanh <soulichanh.kithor@gmail.com>
2020-11-17 14:49:59 +0100

    update the formular contact

--- templates/contact.php
+++ templates/contact.php
@@ -6,27 +6,27 @@
 
           <div class="col-lg-12">
             <div class="section-title mb-5">
-              <h2>Contact Us</h2>
+              <h2>Pour me contacter remplissez ce formulaire</h2>
             </div>
             <form method="post" action="../public/index.php?route=contact">
               
                   <div class="row">
                       <div class="col-md-6 form-group">
-                          <label for="fname">First Name</label>
+                          <label for="fname">Nom</label>
                           <input type="text" id="fname" class="form-control form-control-lg">
                       </div>
                       <div class="col-md-6 form-group">
-                          <label for="lname">Last Name</label>
+                          <label for="lname">Prénom</label>
                           <input type="text" id="lname" class="form-control form-control-lg">
                       </div>
                   </div>
                   <div class="row">
                       <div class="col-md-6 form-group">
-                          <label for="eaddress">Email Address</label>
+                          <label for="eaddress">Addresse email</label>
                           <input type="text" id="eaddress" class="form-control form-control-lg">
                       </div>
                       <div class="col-md-6 form-group">
-                          <label for="tel">Tel. Number</label>
+                          <label for="tel">Téléphone</label>
                           <input type="text" id="tel" class="form-control form-control-lg">
                       </div>
                   </div>
@@ -39,7 +39,7 @@
 
                   <div class="row">
                       <div class="col-12">
-                          <input type="submit" value="Send Message" class="btn btn-primary py-3 px-5">
+                          <input type="submit" value="Envoyez" class="btn btn-primary py-3 px-5">
                       </div>
                   </div>
               
