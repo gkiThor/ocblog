e0b6f7846808bc33684a61a147b92502970b25fe
gsoulichanh <soulichanh.kithor@gmail.com>
2020-11-18 15:44:55 +0100

    update design in the update_password.php file

--- templates/profile.php
+++ templates/profile.php
@@ -1,12 +1,45 @@
 <?php $this->title = 'Mon profil'; ?>
-<h1>Mon blog</h1>
-<p>En construction</p>
-<?= $this->session->show('update_password'); ?>
-<div>
-    <h2><?= $this->session->get('pseudo'); ?></h2>
-    <p><?= $this->session->get('id'); ?></p>
-    <a href="../public/index.php?route=updatePassword">Modifier son mot de passe</a>
-    <a href="../public/index.php?route=deleteAccount">Supprimer mon compte</a>
+<div class="site-section bg-light">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-12">
+                <div class="section-title mb-5 text-center" >
+                    <p>Profile</p>
+                </div>
+                <?= $this->session->show('update_password'); ?>
+                <div class="row">
+                    <div class="col-md-6 form-group">
+                        <div class="section-title mb-5">
+                            <p>Pseudonyme :</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 form-group">
+                        <h2><?= $this->session->get('pseudo'); ?></h2>
+                    </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-md-6 form-group">
+                        <div class="section-title mb-5">
+                            <p>Id :</p>
+                        </div>
+                    </div>
+                    <div class="col-md-6 form-group">
+                        <p><?= $this->session->get('id'); ?></p>
+                    </div>
+                </div>
+                    
+                    <a href="../public/index.php?route=updatePassword">Modifier son mot de passe</a>
+                    <a href="../public/index.php?route=deleteAccount">Supprimer mon compte</a>
+                </div>
+                <br>
+                <div class="text-center"> 
+                    <a href="../public/index.php">Retour à l'accueil</a> 
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
-<br>
-<a href="../public/index.php">Retour à l'accueil</a>
+
+
--- templates/update_password.php
+++ templates/update_password.php
@@ -1,13 +1,41 @@
-<?php $this->title = 'Modifier mot mot de passe'; ?>
-<h1>Mon blog</h1>
-<p>En construction</p>
-<div>
-    <p>Le mot de passe de <?= $this->session->get('pseudo'); ?> sera modifié</p>
-    <form method="post" action="../public/index.php?route=updatePassword">
-        <label for="password">Mot de passe</label><br>
-        <input type="password" id="password" name="password"><br>
-        <input type="submit" value="Mettre à jour" id="submit" name="submit">
-    </form>
-</div>
+<?php $this->title = 'Modifier mot de passe'; ?>
+
 <br>
-<a href="../public/index.php">Retour à l'accueil</a>
+
+
+
+
+<div class="site-section bg-light">
+    <div class="container">
+        <div class="row">
+        
+        <div class="col-lg-12">
+            <div class="section-title mb-5 text-center" >
+                <p>Le mot de passe de <?= $this->session->get('pseudo'); ?> sera modifié</p>
+            </div>
+        </div>
+
+            <div class="col-lg-12">
+                <form method="post" action="../public/index.php?route=updatePassword">
+                    <div class="row">
+                        <div class="col-md-12 form-group">
+                            <label for="password">Mot de passe</label><br>
+                            <input type="password" id="password" name="password" class="form-control form-control-lg"><br>
+                            
+                        </div>
+                    </div>
+
+
+                    <div class="row">
+                        <input type="submit" value="Mettre à jour" id="submit" name="submit" class="btn btn-primary py-3 px-5">
+                    </div>
+   
+                </form>
+                
+                <div class="text-center"> 
+                    <a href="../public/index.php">Retour à l'accueil</a> 
+                </div> 
+            </div>        
+        </div>
+    </div>
+</div>
