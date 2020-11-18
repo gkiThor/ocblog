60bc02b16353c982997c5d5d511a7e682b9ee278
gsoulichanh <soulichanh.kithor@gmail.com>
2020-11-18 11:31:07 +0100

    update design in the register.php file

--- templates/register.php
+++ templates/register.php
@@ -1,15 +1,34 @@
 <?php $this->title = "Inscription"; ?>
-<h1>Mon blog</h1>
-<p>En construction</p>
-<div>
-    <form method="post" action="../public/index.php?route=register">
-        <label for="pseudo">Pseudo</label><br>
-        <input type="text" id="pseudo" name="pseudo" value="<?= isset($post) ? htmlspecialchars($post->get('pseudo')): ''; ?>"><br>
-        <?= isset($errors['pseudo']) ? $errors['pseudo'] : ''; ?>
-        <label for="password">Mot de passe</label><br>
-        <input type="password" id="password" name="password"><br>
-        <?= isset($errors['password']) ? $errors['password'] : ''; ?>
-        <input type="submit" value="Inscription" id="submit" name="submit">
-    </form>
-    <a href="../public/index.php">Retour à l'accueil</a>
+<div class="site-section bg-light">
+    <div class="container">
+        <div class="row">
+
+            <div class="col-lg-12">
+                                <form method="post" action="../public/index.php?route=register">
+                                    <div class="row">
+                                        <div class="col-md-12 form-group">
+                                            <label for="pseudo">Pseudo</label><br>
+                                            <input type="text" id="pseudo" name="pseudo" value="<?= isset($post) ? htmlspecialchars($post->get('pseudo')): ''; ?>" class="form-control form-control-lg"><br>
+                                            <?= isset($errors['pseudo']) ? $errors['pseudo'] : ''; ?>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <div class="col-md-12 form-group">
+                                            <label for="password">Mot de passe</label><br>
+                                            <input type="password" id="password" name="password" class="form-control form-control-lg"><br>
+                                            <?= isset($errors['password']) ? $errors['password'] : ''; ?>
+                                        </div>
+                                    </div>
+
+                                    <div class="row">
+                                        <input type="submit" value="Inscription" id="submit" name="submit" class="btn btn-primary py-3 px-5">
+                                    </div>
+                                </form>
+                    <div class="text-center"> 
+                        <a href="../public/index.php">Retour à l'accueil</a> 
+                    </div>               
+            </div>        
+        </div>
+    </div>
 </div>
